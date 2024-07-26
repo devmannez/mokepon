a5917a0db2476b550a425e0fd3b509f68fc0e116
Manuel Antonio Martinez Sogamoso <97009790+devmannez@users.noreply.github.com>
2024-07-26 10:46:32 -0500

    Update mokepon.js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,10 +1,18 @@
-function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById('boton-mascota')
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-}
+function iniciarJuego(){
+	let botonMascotaJugador=document.getElementById('boton-mascota')
+	botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+	}
 
-function seleccionarMascotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA')
-}
-
-window.addEventListener('load', iniciarJuego)
+function seleccionarMascotaJugador(){
+	let inputHipodoge=document.getElementById('hipodoge')
+	let inputCapipepo=document.getElementById('capipepo')
+	let inputRatigueya=document.getElementById('ratigueya')
+	if(inputHipodoge.checked){
+		alert('Seleccionaste a Hipodoge')
+		}else if(inputCapipepo.checked){
+		alert('Seleccionaste a Capipepo')
+		}else if(inputRatigueya.checked){
+		alert('Seleccionaste a Ratigueya')
+		}else{alert('Selecciona una mascota')}
+	}
+window.addEventListener('load',iniciarJuego)
