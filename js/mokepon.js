0e051c0a706f249ce56c148b38dfdd4c7deca2c6
Manuel Antonio Martinez Sogamoso <97009790+devmannez@users.noreply.github.com>
2024-07-31 14:36:56 -0500

    Update mokepon.js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,64 +1,73 @@
 let ataqueJugador
+let ataqueEnemigo
 
-function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById('boton-mascota')
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-
-    let botonFuego = document.getElementById('boton-fuego')
-    botonFuego.addEventListener('click', ataqueFuego)
-    let botonAgua = document.getElementById('boton-agua')
-    botonAgua.addEventListener('click', ataqueAgua)
-    let botonTierra = document.getElementById('boton-tierra')
-    botonTierra.addEventListener('click', ataqueTierra)
+function iniciarJuego(){
+	let botonMascotaJugador=document.getElementById('boton-mascota')
+	botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+	let botonFuego=document.getElementById('boton-fuego')
+	botonFuego.addEventListener('click',ataqueFuego)
+	let botonAgua=document.getElementById('boton-agua')
+	botonAgua.addEventListener('click',ataqueAgua)
+	let botonTierra=document.getElementById('boton-tierra')
+	botonTierra.addEventListener('click',ataqueTierra)
+}
+	
+function seleccionarMascotaJugador(){
+	let inputHipodoge=document.getElementById('hipodoge')
+	let inputCapipepo=document.getElementById('capipepo')
+	let inputRatigueya=document.getElementById('ratigueya')
+	let spanMascotaJugador=document.getElementById('mascota-jugador')
+	if(inputHipodoge.checked){
+	spanMascotaJugador.innerHTML='Hipodoge'
+	}else if(inputCapipepo.checked){
+	spanMascotaJugador.innerHTML='Capipepo'
+	}else if(inputRatigueya.checked){
+	spanMascotaJugador.innerHTML='Ratigueya'
+	}else{
+	alert('Selecciona una mascota')
+	}
+	seleccionarMascotaEnemigo()
 }
 
-function seleccionarMascotaJugador() {
-    let inputHipodoge = document.getElementById('hipodoge')
-    let inputCapipepo = document.getElementById('capipepo')
-    let inputRatigueya = document.getElementById('ratigueya')
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
-    
-    if (inputHipodoge.checked) {
-        spanMascotaJugador.innerHTML = 'Hipodoge'
-    } else if (inputCapipepo.checked) {
-        spanMascotaJugador.innerHTML = 'Capipepo'
-    } else if (inputRatigueya.checked) {
-        spanMascotaJugador.innerHTML = 'Ratigueya'
-    } else {
-        alert('Selecciona una mascota')
-    }
-
-    seleccionarMascotaEnemigo()
+function seleccionarMascotaEnemigo(){
+	let mascotaAleatoria=aleatorio(1,3)
+	let spanMascotaEnemigo=document.getElementById('mascota-enemigo')
+	if(mascotaAleatoria==1){spanMascotaEnemigo.innerHTML='Hipodoge'
+	}else if(mascotaAleatoria==2){
+	spanMascotaEnemigo.innerHTML='Capipepo'
+	}else{
+	spanMascotaEnemigo.innerHTML='Ratigueya'
+	}
 }
 
-function seleccionarMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1,3)
-    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+function ataqueFuego(){
+	ataqueJugador='FUEGO'
+	ataqueAleatorioEnemigo()
+	}
+	
+function ataqueAgua(){
+	ataqueJugador='AGUA'
+	ataqueAleatorioEnemigo()
+	}
 
-    if (ataqueAleatorio == 1) {
-        spanMascotaEnemigo.innerHTML = 'Hipodoge'
-    } else if (ataqueAleatorio == 2) {
-        spanMascotaEnemigo.innerHTML = 'Capipepo'
-    } else {
-        spanMascotaEnemigo.innerHTML = 'Ratigueya'
-    }
-}
+function ataqueTierra(){
+	ataqueJugador='TIERRA'
+	ataqueAleatorioEnemigo()
+	}
 
-function ataqueFuego() {
-    ataqueJugador = 'FUEGO'
-    alert(ataqueJugador)
-}
-function ataqueAgua() {
-    ataqueJugador = 'AGUA'
-    alert(ataqueJugador)
-}
-function ataqueTierra() {
-    ataqueJugador = 'TIERRA'
-    alert(ataqueJugador)
-}
+function ataqueAleatorioEnemigo(){
+	let ataqueAleatorio=aleatorio(1,3)
+	if(ataqueAleatorio==1){
+		ataqueEnemigo='FUEGO'
+		}else if(ataqueAleatorio==2){
+			ataqueEnemigo='AGUA'
+			}else{
+			ataqueEnemigo='TIERRA'
+			}
+	}
+	
+function aleatorio(min,max){
+	return Math.floor(Math.random()*(max-min+1)+min)
+	}
 
-function aleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-window.addEventListener('load', iniciarJuego)
+window.addEventListener('load',iniciarJuego)
